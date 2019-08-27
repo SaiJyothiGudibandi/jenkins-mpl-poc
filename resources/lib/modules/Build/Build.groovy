8753c723f3de6d9c6223d1a5f6aa876a89dc0173
Murilo Costa <murilo.costa@dtidigital.com.br>
2019-08-27 17:41:47 -0300

    Modulos personalizados de Build e Deploy

--- resources/lib/modules/Build/Build.groovy
+++ resources/lib/modules/Build/Build.groovy
@@ -1,2 +1,3 @@
-echo "Build overridden ${CFG.nomeApp}"
-
+sh "echo 'Construindo imagem do Docker'"
+sh "docker build -t ${CFG.imagem_builder} -f Dockerfile.builder ."
+sh "docker build --build-arg BUILDER=${CFG.imagem_builder} -t ${CFG.imagem} -f Dockerfile ."
--- resources/lib/modules/Publish/Publish.groovy
+++ resources/lib/modules/Publish/Publish.groovy
@@ -1,2 +1,3 @@
-echo "Publicando imagem ${CFG.test}"
-
+sh "echo 'Publicando a imagem'"
+sh "aws ecr get-login --no-include-email --region us-east-1 | sh"
+sh "docker push ${CFG.imagem}"
