889e8666eee43c2c1b8dbfb1e1a6bfa4b709305d
Murilo Costa <murilo.costa@dtidigital.com.br>
2019-08-27 14:44:43 -0300

    Modulos personalizados de Build e Deploy

--- vars/NestedPipeline.groovy
+++ vars/NestedPipeline.groovy
@@ -2,19 +2,18 @@
 def call(body){
     MPLInit()
 
-    node('ec2') {
-
-        // def myRepo = checkout scm
-        // def branch = myRepo.GIT_BRANCH.replace("origin/","").replace("/","-")
-        // def imagem = "${env.ENDPOINT_ECR}/${nomeApp}:v0.1.${env.BUILD_NUMBER}_${branch}_${myRepo.GIT_COMMIT}"
-        // def imagem_builder = "builder_${nomeApp}_${branch}"
-        
-        stage('Build'){
-            MPLModule('Build')
+    pipeline {
+        agent {
+            label 'ec2'
         }
+        stages{
+            stage('Build'){
+                MPLModule('Build')
+            }
 
-        stage('Deploy') {
-            MPLModule('Deploy')
+            stage('Deploy') {
+                MPLModule('Deploy')
+            }
         }
     }
 }
