5704e9c4930674ae262d29a4a2c934709b26d255
Murilo Costa <murilo.costa@dtidigital.com.br>
2019-08-27 12:52:24 -0300

    Modulos personalizados de Build e Deploy

--- resources/lib/modules/Build/Build.groovy
+++ resources/lib/modules/Build/Build.groovy
@@ -1,1 +1,1 @@
-echo "Build overridden"
+echo "Build overridden ${CFG.nomeApp}"
--- vars/NestedPipeline.groovy
+++ vars/NestedPipeline.groovy
@@ -4,10 +4,10 @@ def call(body){
 
     node('ec2') {
 
-        def myRepo = checkout scm
-        def branch = myRepo.GIT_BRANCH.replace("origin/","").replace("/","-")
-        def imagem = "${env.ENDPOINT_ECR}/${CFG.nomeApp}:v0.1.${env.BUILD_NUMBER}_${branch}_${myRepo.GIT_COMMIT}"
-        def imagem_builder = "builder_${CFG.nomeApp}_${branch}"
+        // def myRepo = checkout scm
+        // def branch = myRepo.GIT_BRANCH.replace("origin/","").replace("/","-")
+        // def imagem = "${env.ENDPOINT_ECR}/${nomeApp}:v0.1.${env.BUILD_NUMBER}_${branch}_${myRepo.GIT_COMMIT}"
+        // def imagem_builder = "builder_${nomeApp}_${branch}"
         
         stage('Build'){
             MPLModule('Build')
