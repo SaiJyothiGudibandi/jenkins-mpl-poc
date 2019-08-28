d4621136efd4044127f6d2c1492d95beae2eb873
Murilo Costa <murilo.costa@dtidigital.com.br>
2019-08-28 18:49:35 -0300

    Deploy com Liquibase

--- resources/com/prodap/mpl/modules/Deploy/DeployLiquibase.groovy
+++ resources/com/prodap/mpl/modules/Deploy/DeployLiquibase.groovy
@@ -1,5 +1,4 @@
-def liquibaseImagem = "548923902696.dkr.ecr.us-east-1.amazonaws.com/liquibase:latest"
+def liquibaseImagem = "${env.ENDPOINT_ECR}/liquibase:latest"
 def host = "env.CONNECTION_STRING_${CFG.ambiente.toUpperCase()}"
 def password =  "env.PASS_${CFG.ambiente.toUpperCase()}"
-sh "docker create"
 sh "docker run --rm -v ${CFG.volume} -e LIQUIBASE_HOST=${host} -e LIQUIBASE_USERNAME=jenkins -e LIQUIBASE_PASSWORD=${password} -e LIQUIBASE_PASSWORD=${CFG.database} -e LIQUIBASE_PORT=3306 ${liquibaseImagem} liquibase updateToTag 1.17"
