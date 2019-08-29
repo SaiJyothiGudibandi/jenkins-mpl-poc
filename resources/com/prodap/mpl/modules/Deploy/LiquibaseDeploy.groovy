a9fa4ce9ed079dafd5ba7cd9d13292be6fbedd69
Murilo Costa <murilo.costa@dtidigital.com.br>
2019-08-29 13:15:00 -0300

    Parametro liquibase no deploy

--- resources/com/prodap/mpl/modules/Deploy/LiquibaseDeploy.groovy
+++ resources/com/prodap/mpl/modules/Deploy/LiquibaseDeploy.groovy
@@ -2,7 +2,7 @@ def liquibaseImagem = "${env.ENDPOINT_ECR}/liquibase:latest"
 def host = CFG.'liquibase.host'
 def password = CFG.'liquibase.password'
 def username = CFG.'liquibase.username'
-def volume = CFG.'liquibase.volume' ? CFG.'liquibase.volume' : 'fonte/db/changelogs'
+def volume = CFG.'liquibase.volume' ? CFG.'liquibase.volume' : 'src/db/changelogs'
 def db = CFG.'liquibase.database'
 def tag = CFG.'liquibase.tag'
-sh "docker run --rm -v ${volume} -e LIQUIBASE_HOST=${host} -e LIQUIBASE_USERNAME=${username} -e LIQUIBASE_PASSWORD=${password} -e LIQUIBASE_DATABASE=${db} -e LIQUIBASE_PORT=3306 ${liquibaseImagem} liquibase updateToTag ${tag}"
+sh "docker run --rm -v ${volume}:/workspace/src/db/changelogs -e LIQUIBASE_HOST=${host} -e LIQUIBASE_USERNAME=${username} -e LIQUIBASE_PASSWORD=${password} -e LIQUIBASE_DATABASE=${db} -e LIQUIBASE_PORT=3306 ${liquibaseImagem} liquibase updateToTag ${tag}"
