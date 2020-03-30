48b5614f74845791b58c0f14c2f93d1f26068d93
SaiJyothi Gudibandi <saijyothigudibandi9@gmail.com>
2020-03-30 00:23:08 -0700

    Comminting the new changes

--- .jenkins/modules/Deploy/DockerDeploy.groovy
+++ .jenkins/modules/Deploy/DockerDeploy.groovy
@@ -2,8 +2,8 @@
  * Deploy the built Docker images
  */
 
-sh "sudo docker push mlptest"
+sh "sudo docker push docker push saijyothi9/mlptest"
 
 sh "docker stop \$(docker ps -a -q)"
 sh "docker rm \$(docker ps -a -q)"
-sh "docker run --name mynginx1 -p 80:80 -d mlptest"
+sh "docker run --name mynginx1 -p 80:80 -d saijyothi9/mlptest"
