5e3651b8c5f8066d778679b3fc888893ee75194d
SaiJyothi Gudibandi <saijyothigudibandi9@gmail.com>
2020-03-29 22:34:10 -0700

    New changes

--- .jenkins/modules/Build/DockerBuild.groovy
+++ .jenkins/modules/Build/DockerBuild.groovy
@@ -1,16 +1,1 @@
-def call(sudo = true, extraTags = []) {
-
-  prefix = ""
-  if (sudo) {
-    prefix = "sudo "
-  }
-  sh """${prefix} docker image build \
-        -t Test-Jenkins-MPL ."""
-  echo "Docker BUild"
-
-  sh """${prefix} docker login"""
-
-  sh """${prefix} docker image push \
-        Test-Jenkins-MPL"""
-
-}
+echo ""Docker build started""
